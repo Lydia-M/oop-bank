48a9f854b143b355000af6882803e94c2dcfa5fa
Lydia Mansfield <lydia.m.mansfield@gmail.com>
2017-10-11 23:37:36 +0000

    Tests for deposit, withdrawal, and tranfer methods are now passing.

--- bank_account.rb
+++ bank_account.rb
@@ -2,7 +2,21 @@ class BankAccount
 	attr_reader :balance
 
 	def initialize(opening_balance, account_holder)
-	 @balance = opening_balance
+	    @balance = opening_balance
 	end
+    
+    def deposit(amount)
+        @balance += amount # += is the same thing as saying equals @balance + amount
+    end
+    
+    def withdraw(amount)
+        @balance -= amount
+    end
+    
+    def transfer(amount, account)
+        withdraw(amount)
+        account.deposit(amount)
+    end
+    
 end
 
